a7f7ead92e0fca7e79617ad2c5a9ec3d873a8caf
Keith McHugh <mchughkeith3@gmail.com>
2022-11-22 09:52:20 -0500

    Add project files.

---
+++ CS_330_3489/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+namespace CS_330_3489
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MessageBox.Show("What up World?");
+
+            #region Sample Read/Write from Text File.
+            string[] question_list = System.IO.File.ReadAllLines(@"C:\Users\sekol_michael\Desktop\CS_Projects\BPA_State_2022\BPA_State_2022\quizquestion.txt");
+
+            MessageBox.Show(question_list[2]);
+
+            foreach (string line in question_list)
+            {
+                Trace.WriteLine(line);
+            }
+
+            #endregion
+
+        }
+    }
+}
